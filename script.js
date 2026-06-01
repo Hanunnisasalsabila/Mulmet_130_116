d7e778c966131be9790e986b950e1b8df3dd7feb
Hanunnisasalsabila <hanunsalsabila83@gmail.com>
2026-06-01 20:42:47 +0700

    Scene 2

--- script.js
+++ script.js
@@ -1,3 +1,41 @@
-function mulaiPetualangan() {
-    alert("Tombol berhasil diklik! Tinggal lanjut bikin Scene 2 nih.");
+// Variabel untuk menyimpan data pemain
+let namaKapten = "";
+let avatarPilihan = "";
+
+// Fungsi pindah antar Scene
+function pindahScene(nomorScene) {
+    // Sembunyikan semua
+    let semuaScene = document.querySelectorAll('.scene');
+    semuaScene.forEach(scene => {
+        scene.classList.add('hidden');
+    });
+
+    // Munculkan yang dituju
+    document.getElementById('scene-' + nomorScene).classList.remove('hidden');
+}
+
+// Fungsi memberikan highlight saat gambar avatar diklik
+function pilihAvatar(jenis) {
+    avatarPilihan = jenis;
+    
+    // Hapus efek hijau dari dua-duanya dulu
+    document.getElementById('card-cowok').classList.remove('selected');
+    document.getElementById('card-cewek').classList.remove('selected');
+    
+    // Tambahkan efek hijau ke yang dipilih
+    document.getElementById('card-' + jenis).classList.add('selected');
+}
+
+// Fungsi mengecek nama dan avatar sebelum lanjut ke Scene 3
+function cekDataLanjut() {
+    namaKapten = document.getElementById('input-nama').value;
+    
+    if (namaKapten === "") {
+        alert("Eh, Kapten lupa mengisi nama nih!");
+    } else if (avatarPilihan === "") {
+        alert("Pilih karakter Kapten Cewek atau Cowok dulu ya!");
+    } else {
+        alert("Siap Kapten " + namaKapten + "! Lanjut ke ruang ganti seragam.");
+        // pindahScene(3); // Nanti diaktifkan kalau Scene 3 sudah kita buat
+    }
 }
